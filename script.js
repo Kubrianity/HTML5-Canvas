ee85485c5aec25b31378a280529730827ad9e38b
Kubrianity <kubraaozerenn@gmail.com>
2022-06-23 20:52:01 +0300

    Update mousemove event changing stroke color and width

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "blue";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = 0;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if(!isDrawing) return; //stops the fn when they are not moused down 
+
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX,lastY);
@@ -19,9 +24,22 @@ function draw(e) {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if(hue >= 360) {
+        hue = 0;
+    }
+    if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+    direction ? ctx.lineWidth++ : ctx.lineWidth--;
 }
 
-canvas.addEventListener("mousedown", () => isDrawing = true);
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener("mouseup", () => isDrawing = false);
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseout", () => isDrawing = false);
